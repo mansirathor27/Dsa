8d855240a2e432522b5d00cf98ef672b43fad8b2
mansirathor27 <mansirathor575@gmail.com>
2025-12-09 19:48:03 +0530

    Solved 219 Contains Duplicates II

---
+++ Arrays/SlidingWindow/containsDuplicates.java
@@ -0,0 +1,20 @@
+// LeetcArrays.SlidingWindowgWindowains Duplicate II
+
+package Arrays.SlidingWindow;
+import java.util.*;
+
+class Solution{
+    public boolean containsDuplicates(int[] nums, int k){
+        HashSet<Integer> window = new HashSet<>();
+        for(int i =0; i< nums.length; i++){
+            if(window.contains(nums[i])){
+                return true;
+            }
+            window.add(nums[i]);
+            if(window.size()>k){
+                window.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
