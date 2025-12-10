40813c2a503778100bcd50da0936680f30cc6c57
mansirathor27 <mansirathor575@gmail.com>
2025-12-10 15:43:06 +0530

    Solved 2269 Find the K-Beauty of a Number

---
+++ Arrays/SlidingWindow/KBeautyOfNumber.java
@@ -0,0 +1,16 @@
+package Arrays.SlidingWindow;
+
+public class KBeautyOfNumber {
+    public int divisorSubstrings(int num , int k){
+        String s = String.valueOf(num);
+        int count =0;
+        for(int i =0; i+k <= s.length();i++){
+            String sub = s.substring(i, i+k);
+            int val = Integer.parseInt(sub);
+            if(val != 0 && num%val == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
