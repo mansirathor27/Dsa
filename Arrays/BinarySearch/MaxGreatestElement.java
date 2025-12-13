e165a4a3f37def55df944211cf336beedc5084b8
mansirathor27 <mansirathor575@gmail.com>
2025-12-13 12:15:54 +0530

    Solved 744 Find Smallest Letter Greater Than Target

---
+++ Arrays/BinarySearch/MaxGreatestElement.java
@@ -0,0 +1,20 @@
+package Arrays.BinarySearch;
+
+public class MaxGreatestElement {
+    public char maxGreatestElement( char[] letters, char target){
+        int left = 0;
+        int right = letters.length -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(letters[mid] <= target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        if(left == letters.length){
+            return letters[0];
+        }
+        return letters[left];
+    }
+}
