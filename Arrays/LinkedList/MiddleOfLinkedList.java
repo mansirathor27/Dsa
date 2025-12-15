f2aa40b37651e9be057b3d805e10da4f9af60cbd
mansirathor27 <mansirathor575@gmail.com>
2025-12-15 20:40:32 +0530

    Solved 876 Middle of the Linked List

---
+++ Arrays/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,26 @@
+package Arrays.LinkedList;
+
+
+class Node{
+    int val;
+    Node next;
+    Node(){}
+    Node(int val){
+        this.val = val;
+    }
+    Node(int val, Node next){
+        this.val = val;
+        this.next = next;
+    }
+}
+public class MiddleOfLinkedList {
+    public Node middleNode(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
