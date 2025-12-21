ddc382863bc884d56e8491137379de5f3e60c134
mansirathor27 <mansirathor575@gmail.com>
2025-12-21 12:53:48 +0530

    Solved 225 Implement Stack using Queues

---
+++ Stacks/ImplementStackUsingQueue.java
@@ -0,0 +1,23 @@
+package Stacks;
+import java.util.*;
+public class ImplementStackUsingQueue {
+    Queue<Integer> q;
+    public ImplementStackUsingQueue(){
+        q = new LinkedList<>();
+    }
+    public void push(int x){
+        q.add(x);
+        for(int i=0; i< q.size() -1; i++){
+            q.add(q.remove());
+        }
+    }
+    public int pop(){
+        return q.remove();
+    }
+    public int peek(){
+        return q.peek();
+    }
+    public boolean empty(){
+        return q.isEmpty();
+    }
+}
