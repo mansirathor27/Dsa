135f6eb5ce3e0e33e433f799b3ad10a5a8993040
mansirathor27 <mansirathor575@gmail.com>
2025-12-25 12:32:06 +0530

    Solved 150 Evaluate Reverse Polish Notation

---
+++ Stacks/EvalRevPolishNotation.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class EvalRevPolishNotation {
+    public int evalRPN(String[] tokens){
+        Stack<Integer> st = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a+b);
+            }else if(token.equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a-b);
+            }else if(token.equals("*")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a*b);
+            }else if(token.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a/b);
+            }else{
+                st.push(Integer.parseInt(token));
+            }
+        }
+        return st.pop();
+    }
+}
